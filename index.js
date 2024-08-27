ec4477bdfb54401ffd5d389d75b19c39cc2a5504
Madyson-Webb <maddy.6725@gmail.com>
2024-08-27 15:41:30 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log("file loaded");
+
+$(".navButton").on('click', function(event){
+
+    
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+    
+        // Store hash
+        var hash = this.hash;
+    
+        // Using jQuery's animate() method to add smooth page scroll
+        $('html, body').animate({scrollTop: $(hash).offset().top}, 200, function(){
+        
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+        });
+    }
+});
